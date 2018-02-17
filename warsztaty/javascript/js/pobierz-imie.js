35174e6dbeba8f2134bbd4ba0baec1519a0d5f51
Michał Borkowski <michal88borkowski@gmail.com>
2018-02-17 17:00:04 +0100

    Warsztaty z javascript 1

---
+++ warsztaty/javascript/js/pobierz-imie.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function pobierzImie(Event) {
+    
+    event.preventDefault();
+    var form = document.getElementById("formularz");
+    
+    
+    
+    
+    console.log("hula czy nie");
+}
---
+++ warsztaty/javascript/js/ustaw-tlo.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function ustawTlo() {
+    console.log("dziala");
+    var allParagraphs = document.getElementsByTagName("p");
+    allParagraphs[0].style.backgroundColor = "green";
+    allParagraphs[1].style.backgroundColor = "violet";
+}
+//sa na to jeszcze dwa sposoby
